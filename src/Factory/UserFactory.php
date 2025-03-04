af48bbbd8a5237b6a14961e3680774da357097b5
Junior Djemba <junior.djemba@eemi.com>
2025-03-04 12:22:58 +0100

    feat(users): add fixtures

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<User>
+ */
+final class UserFactory extends PersistentProxyObjectFactory
+{
+    private const USER_ROLES = [
+        User::ROLE_USER,
+        User::ROLE_ADMIN
+    ];
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'email' => self::faker()->email(),
+            'password' => 'aqwzsxedc',
+            'roles' => self::faker()->randomElement([self::USER_ROLES]),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this// ->afterInstantiate(function(User $user): void {})
+            ;
+    }
+}
