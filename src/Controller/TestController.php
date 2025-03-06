aa805672cf86d748261a9a412d2e6d99a7c777a5
Mathéo SAITER <99892446+Sukeshi7@users.noreply.github.com>
2025-03-06 20:11:07 +0100

    first conflict resolve

---
+++ src/Controller/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+Namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+	#[Route('/admin', name: 'test')]
+	public function test():Response
+	{
+		//return new Response('Bonjour');
+		return $this->render('/admin.html.twig');
+	}
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     './templates/**/*.html.twig',
+    './assets/**/*.js',
   ],
   theme: {
     extend: {},
