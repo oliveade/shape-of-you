c2731534eab40e16033f88b3782133243d386277
Mathéo SAITER <99892446+Sukeshi7@users.noreply.github.com>
2025-03-07 10:13:37 +0100

    login template update

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-Namespace App\Controller;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,17 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class Controller extends AbstractController
 {
     #[Route('/addimage', name: 'app_upload_photo')]
-    public function test():Response
+    public function test(): Response
     {
         return $this->render('/photos.html.twig');
     }
+
     #[Route('/', name: 'app_index')]
-    public function home():Response
+    public function home(): Response
     {
         return $this->render('/index.html.twig');
     }
+
     #[Route('/accueil', name: 'app_home')]
-    public function darshboardUser():Response
+    public function darshboardUser(): Response
     {
         return $this->render('/home.html.twig');
     }
