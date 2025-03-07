ad95237ac05bba26dad36386284066466c01f219
Junior Djemba <junior.djemba@eemi.com>
2025-03-07 13:58:34 +0100

    feat(users): add userfixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        UserFactory::new()->isAdmin()->create([
+            'username' => 'petitbiscuit',
+            'email' => 'junior@resend.com',
+            'deleted' => false,
+        ]);
+
+        UserFactory::new()->isAdmin()->create([
+            'username' => 'sukeshi',
+            'email' => 'matheo@resend.com',
+            'deleted' => false,
+        ]);
+
+        UserFactory::new()->isAdmin()->create([
+            'username' => 'gratias',
+            'email' => 'olive@resend.com',
+            'deleted' => false,
+        ]);
+
+        UserFactory::new()->createMany(30);
+    }
+}
---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<User>
+ */
+final class UserFactory extends PersistentProxyObjectFactory
+{
+    private $passwordHasher;
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    public function isAdmin(): self
+    {
+        return $this->with([
+            'roles' => [User::ROLE_ADMIN],
+        ]);
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'username' => self::faker()->userName(),
+            'email' => self::faker()->unique()->safeEmail(),
+            'password' => 'azeqsdwxc',
+            'birthdate' => \DateTimeImmutable::createFromMutable(self::faker()->dateTimeBetween('-50 year', 'now')),
+            'roles' => [User::ROLE_USER],
+            'deleted' => self::faker()->boolean(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            ->afterInstantiate(function (User $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            })
+        ;
+    }
+}
