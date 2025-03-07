27e35e9fd06668c766029de630a15b361df22dc6
Mathéo SAITER <99892446+Sukeshi7@users.noreply.github.com>
2025-03-07 10:58:53 +0100

    fix login and routes conflicts

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -19,5 +21,11 @@ class LoginController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+	
+    #[Route('/redirect-after-login', name: 'redirect_after_login')]
+    public function redirectAfterLogin(Security $security): RedirectResponse
+    {
+        return $this->redirectToRoute('app_dashboard');
     }
 }
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Dto\PasswordUpdateDto;
+use App\Entity\User;
 use App\Form\PasswordUpdateType;
 use App\Form\UserSelfEditType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,54 +22,64 @@ final class ProfileController extends AbstractController
     #[Route('/', name: 'app_profile')]
     public function index(Request $request, EntityManagerInterface $entityManager, Security $security, UserPasswordHasherInterface $passwordHasher): Response
     {
-		$user = $security->getUser();
+        $user = $security->getUser();
 
-		$passwordUpdateDto = new PasswordUpdateDto();
+        $passwordUpdateDto = new PasswordUpdateDto();
 		
-		$form = $this->createForm(PasswordUpdateType::class, $passwordUpdateDto);
-		$form->handleRequest($request);
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdateDto);
+        $form->handleRequest($request);
 		
-		if ($form->isSubmitted() && $form->isValid()) {
-			if (!password_verify($passwordUpdateDto->oldPassword, $user->getPassword())) {
-				$this->addFlash('error', 'Ancien mot de passe incorrect.');
-				dump('Ancien mot de passe incorrect.');
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!password_verify($passwordUpdateDto->oldPassword, $user->getPassword())) {
+                $this->addFlash('error', 'Ancien mot de passe incorrect.');
+                dump('Ancien mot de passe incorrect.');
+            } elseif ($passwordUpdateDto->newPassword !== $passwordUpdateDto->confirmPassword) {
+                $this->addFlash('error', 'Les nouveaux mots de passe ne correspondent pas.');
+                dump('Les nouveaux mots de passe ne correspondent pas.');
+            } else {
+                dump('Mot de passe mis à jour avec succès.');
+                $hashedPassword = $passwordHasher->hashPassword($user, $passwordUpdateDto->newPassword);
+                $user->setPassword($hashedPassword);
+                $entityManager->flush();
 				
-			} elseif ($passwordUpdateDto->newPassword !== $passwordUpdateDto->confirmPassword) {
-				$this->addFlash('error', 'Les nouveaux mots de passe ne correspondent pas.');
-				dump('Les nouveaux mots de passe ne correspondent pas.');
-			} else {
-				dump('Mot de passe mis à jour avec succès.');
-				$hashedPassword = $passwordHasher->hashPassword($user, $passwordUpdateDto->newPassword);
-				$user->setPassword($hashedPassword);
-				$entityManager->flush();
-				
-				$this->addFlash('success', 'Mot de passe mis à jour avec succès.');
-				//return $this->redirectToRoute('app_profile');
-			}
-		}
+                $this->addFlash('success', 'Mot de passe mis à jour avec succès.');
+                // return $this->redirectToRoute('app_profile');
+            }
+        }
 		
         return $this->render('profile/index.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 	
-	#[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
-	public function edit(Request $request, EntityManagerInterface $entityManager, Security $security): Response
-	{
-		$user = $security->getUser();
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager, Security $security): Response
+    {
+        $user = $security->getUser();
 		
-		$form = $this->createForm(UserSelfEditType::class, $user);
-		$form->handleRequest($request);
+        $form = $this->createForm(UserSelfEditType::class, $user);
+        $form->handleRequest($request);
 		
-		if ($form->isSubmitted() && $form->isValid()) {
-			$entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
 			
-			return $this->redirectToRoute('app_profile', [], Response::HTTP_SEE_OTHER);
-		}
+            return $this->redirectToRoute('app_profile', [], Response::HTTP_SEE_OTHER);
+        }
 		
-		return $this->render('profile/edit.html.twig', [
-				'user' => $user,
-				'form' => $form,
-		]);
-	}
+        return $this->render('profile/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+	
+    #[Route('/{id}', name: 'app_profile_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+		
+        return $this->redirectToRoute('app_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
