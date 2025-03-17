c8a8d9952c3976ef12b54fb365e2d338bdc32480
Junior Djemba <junior.djemba@eemi.com>
2025-03-17 14:05:32 +0100

    respond matheo's feedback

--- deploy.php
+++ deploy.php
@@ -14,9 +14,26 @@ add('writable_dirs', []);
 // Hosts
 
 host('161.35.162.159')
+    ->setLabels(['stage' => 'prod'])
     ->set('remote_user', 'master_mmcdasburj')
     ->set('deploy_path', '~/applications/zdjmyznepe/public_html');
 
+// Tasks
+
+//task('deploy:secrets', function () {
+//    upload(getenv('DOTENV'), '{{deploy_path}}/shared/.env');
+//});
+
+//task('build', function () {
+//    cd('{{release_path}}');
+//    run('npm install');
+//    run('npm run prod');
+//});
+
 // Hooks
+
+// after('deploy:update_code', 'deploy:secrets');
+
+//after('deploy:update_code', 'build');
 
 after('deploy:failed', 'deploy:unlock');
