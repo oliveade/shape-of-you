783c55cd86a9f24d16f33724daadf575547fe63c
Junior Djemba <junior.djemba@eemi.com>
2025-03-17 02:05:46 +0100

    cd: add deployer

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+
+set('repository', 'git@github.com:/oliveade/shape-of-you.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('shape-of-you.xyz')
+    ->set('remote_user', 'master_mmcdasburj')
+    ->set('deploy_path', '~/applications/zdjmyznepe/public_html');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
